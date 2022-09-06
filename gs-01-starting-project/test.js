17a1936cf2be2028b083e796993a536f24196079
Ajose Tosin <tosinemidastouch@gmail.com>
2022-09-06 19:15:50 +0100

    Starting advance vue course

---
+++ gs-01-starting-project/test.js
@@ -0,0 +1,45 @@
+// let nameX = "Chioma";
+// console.log(nameX);
+
+// let nameX = "Joseph";
+
+// var nameY;
+
+// console.log(nameY);
+// var nameY = "Chioma";
+
+// x = 5;
+// var x;
+// console.log(x);
+
+// var nameX = "Joseph";
+
+// let nameX;
+// console.log(nameX);
+// let nameX = "Chioma";
+
+let myName = "Obi";
+
+function callHerName() {
+  let myBestFriend = "Joseph";
+  console.log(myName);
+  console.log(myBestFriend);
+}
+
+console.log(callHerName());
+
+let chiomaData = {
+  name: "Chioma",
+  lastName: "Franca",
+  age: 25,
+  maritalStatus: "single",
+};
+
+// let newChiomaData;
+chiomaData.name = "Felix";
+chiomaData.age = 40;
+// console.log(chiomaData.name, chiomaData.age);
+chiomaData = {
+  name: "Tosin",
+};
+console.log(chiomaData.name, chiomaData.age);
